1790379c952888a2f28c3789cb012c57b3c8cf74
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2022-04-07 23:02:14 +0000

    Update src

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/sidebar.css'
+import Twitter from "@material-ui/icons/Twitter"
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <Twitter />
+        </div>
+    )
+}
+
+export default Sidebar
