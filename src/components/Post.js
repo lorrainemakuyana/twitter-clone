118d3fb4d9ace81986f856473c5c1179b3c1988f
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2022-04-12 21:59:08 +0000

    Add Post component

---
+++ src/components/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Post.css'
+
+function Post( { avatar, displayName, username, verified, text, image } ) {
+	return (
+		<div>
+
+		</div>
+	)
+}
+
+export default Post
